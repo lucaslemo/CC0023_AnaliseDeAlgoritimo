91be339661b08325b43b7ef085c674b91a216fff
LucasLemo <lucas.lemos@aluno.ufca.edu.br>
2021-11-09 11:26:55 -0300

    Corrigido divisao do vetor

--- Q01.c
+++ Q01.c
@@ -3,17 +3,17 @@
 #include <math.h>
 
 
-int func(int *vetor, int inicio, int fim)
+long int func(long int *vetor, long int inicio, long int fim)
 {
-    int mid = floor((fim - inicio)/2);
+    long int mid = floor((fim + inicio)/2);
     if (inicio == fim)
-        return inicio+1;
+        return inicio;
     if (vetor[inicio] >= vetor[inicio+1])
-        return inicio+1;
+        return inicio;
     if (vetor[fim] >= vetor[fim-1])
-        return fim+1;
+        return fim;
     if (vetor[mid-1] <= vetor[mid] && vetor[mid] >= vetor[mid+1])
-        return mid+1;
+        return mid;
 
     if (vetor[mid-1] > vetor[mid])
         return func(vetor, inicio, mid);
@@ -24,31 +24,31 @@ int func(int *vetor, int inicio, int fim)
 int main()
 {
     // Inicializa as variaveis
-    int qtdTestes;
-    int *tamanhos;
-    int **vetores;
+    long int qtdTestes;
+    long int *tamanhos;
+    signed long int **vetores;
 
     // Le as entradas
     setbuf(stdin, NULL);
-    scanf("%d", &qtdTestes);
-    tamanhos = (int*)malloc(qtdTestes * sizeof(int));
-    vetores = (int**)malloc(qtdTestes * sizeof(int*));
+    scanf("%ld", &qtdTestes);
+    tamanhos = (long int*)malloc(qtdTestes * sizeof(long int));
+    vetores = (signed long int**)malloc(qtdTestes * sizeof(signed long int*));
     for (int i = 0; i < qtdTestes; i++){
-        scanf("%d", &tamanhos[i]);
-        vetores[i] = (int*)malloc(tamanhos[i] * sizeof(int));
-        for (int n = 0; n < tamanhos[i]; n++){
-            scanf("%d", &vetores[i][n]);
+        scanf("%ld", &tamanhos[i]);
+        vetores[i] = (signed long int*)malloc(tamanhos[i] * sizeof(signed long int));
+        for (long int n = 0; n < tamanhos[i]; n++){
+            scanf("%ld", &vetores[i][n]);
         }
     }
 
     // Chama funcao recursiva para todos os casos
-    for (int i = 0; i < qtdTestes; i++){
-        printf("%d\n", func(vetores[i], 0, tamanhos[i] - 1));
+    for (long int i = 0; i < qtdTestes; i++){
+        printf("%ld\n", func(vetores[i], 0, tamanhos[i] - 1) + 1);
     }
 
     // Liberando memoria alocada
     free(tamanhos);
-    for (int i = 0; i < qtdTestes; i++){
+    for (long int i = 0; i < qtdTestes; i++){
         free(vetores[i]);
     }
     free(vetores);
